aee23de8c1160494a18eb7f8ad0c3b545121e3c1
MBHunter88 <chellehunt88@gmail.com>
2024-10-19 15:04:39 -0700

    update user profile display using antd

--- client/src/pages/UserProfile.jsx
+++ client/src/pages/UserProfile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import { useUser } from '../context/UserContext'
+import { Card } from 'antd';
 
 
 const UserProfile = () => {
@@ -14,8 +15,26 @@ const UserProfile = () => {
     return (
         <div className={'user-profile'}>
             <NavBar />
-            <h1>Welcome {user.name}</h1>
+            <Card
+            title="Profile">
+            <h2>Welcome {user.name} ({user.pronouns})</h2>
+            <Card type='inner' 
+            title='User Details'>
             <p>Email: {user.email}</p>
+            <p>Location: {user.location}</p>
+            <p>Family Building Plan: {user.family_structure}</p>
+            <p>Age: {user.age}</p>
+            </Card>
+            <Card type='inner' title="Partner Details">
+            <p>Partner Name: {user.partner_name} ({user.partner_pronouns})</p>
+            <p>Partner Identifies As: {user.partner_identifies_as}</p>
+            <p>Partner Age: {user.partner_age}</p>
+            </Card>
+            <Card type='inner'
+            title='Conception Plan'>
+            <p>{user.generated_plan}</p>
+            </Card>
+            </Card>
         </div>
     );
 };
