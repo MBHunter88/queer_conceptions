f08a7c6b42a11a0c215351f5985b82a031dc3519
MBHunter88 <130544940+MBHunter88@users.noreply.github.com>
2024-10-24 20:49:05 -0700

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,15 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   test: {
     environment: 'jsdom', 
     setupFiles: ['/src/setupTest.js'],
-  },
-  server: {
-    proxy: {
-      host: '127.0.0.1', 
-      port: 8080
-    },
   },
 })
