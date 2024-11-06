94a93aafad65dde257d454a5f8e4bd74507ef9d7
MBHunter88 <chellehunt88@gmail.com>
2024-11-05 20:04:13 -0800

    update chat page, addidng image and using same styling format for other page components

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from 'react';
-import { Typography, Button, Layout, Row, Col, Modal } from 'antd';
+import { Typography, Button, Layout, Row, Col, Modal, Divider } from 'antd';
 import Chatbot from '../components/ChatBot';
 import { useUser } from '../context/UserContext';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
 
-const { Content } = Layout;
+const { Content, Footer } = Layout;
 const { Title, Text } = Typography;
 
 const ChatPage = () => {
-    const { user, setUser } = useUser();
-    const [showChat, setShowChat] = useState(false)
+    const { user } = useUser();
+    const [showChat, setShowChat] = useState(false);
     const navigate = useNavigate();
 
-     //user must be logged in to use chat feature
-     const handleShowChat = () => {
+    // User must be logged in to use chat feature
+    const handleShowChat = () => {
         if (!user) {
           // Show warning modal if the user is not logged in
           Modal.warning({
@@ -29,38 +29,101 @@ const ChatPage = () => {
       };
 
     return (
-        <Layout>
-            <Content style={{ padding: '30px', maxWidth: '100%', margin: '0 auto', backgroundColor: '#EEE0CB',borderRadius: '8px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)' }}>
-                
-            <Row gutter={[0, 40]} justify="center">
-                    <Title level={2} style={{ textAlign: 'center', fontWeight: 'bold', marginBottom: '20px' }}>Virtual Doula</Title>
-                    <Text style={{ fontSize: '1.2rem', lineHeight: '1.75', textAlign: 'center', maxWidth: '100%', marginBottom: '20px' }}>
-                    Welcome to the Virtual Doula Chatbot! This chatbot is designed to 
-                    support you by answering your questions related to family planning, 
-                    conception methods, legal considerations, and more. Simply type your 
-                    question in the input box below, and our Virtual Doula will respond 
-                    with informative and helpful guidance. Remember, all information 
-                    provided is for educational purposes and does not replace professional
-                    medical or legal advice. Let's get started – ask anything you'd like to know!
-                    </Text>
-                    <Col span={24} style={{ textAlign: 'center' }}>
-                   {user ? (
-                     <Chatbot />
-                   ) : (
-                     <Button type="primary" onClick={handleShowChat} style={{ padding: '10px 20px', fontSize: '1.2rem', backgroundColor: '#007000', borderColor: '#007000' }}>
-                       Chat Now
-                     </Button>
-                   )}
+        <Layout style={{ backgroundColor: '#EEE0CB', padding: '50px 0' }}>
+            <Content style={{ padding: '0 20px' }}>
+                {/* Hero Section */}
+                <Row gutter={[0, 40]} justify="center">
+                    <div
+                        style={{
+                            height: 'auto',
+                            padding: '50px 20px',
+                            background: 'url(chat.png) no-repeat center/cover',
+                            color: '#fff',
+                            position: 'relative',
+                            textShadow: '2px 2px 5px rgba(0, 0, 0, 0.7)',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            width: '100vw',
+                            margin: '0 -20px',
+                        }}
+                    >
+                        <div
+                            style={{
+                                position: 'absolute',
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                background: 'rgba(0, 0, 0, 0.5)',
+                            }}
+                        ></div>
+                        <div
+                            style={{
+                                position: 'relative',
+                                zIndex: 1,
+                                maxWidth: '70%',
+                                textAlign: 'justify',
+                            }}
+                        >
+                            <Title
+                                level={1}
+                                style={{
+                                    fontWeight: 'bold',
+                                    color: '#fff',
+                                }}
+                            >
+                                Virtual Doula
+                            </Title>
+                            <Text
+                                style={{
+                                    fontSize: '1.75rem',
+                                    lineHeight: '1.5',
+                                    fontWeight: '500',
+                                    color: 'white',
+                                    textAlign: 'justify',
+                                }}
+                            >
+                                Welcome to the Virtual Doula Chatbot! This chatbot is designed to support you by
+                                answering your questions related to family planning, conception methods, legal
+                                considerations, and more. Simply type your question in the input box below, and our
+                                Virtual Doula will respond with informative and helpful guidance. Remember, all
+                                information provided is for educational purposes and does not replace professional
+                                medical or legal advice. Let's get started – ask anything you'd like to know!
+                            </Text>
+                        </div>
+                    </div>
+
+                    <Divider />
+
+                    {/* Chatbot Section */}
+                    <Col span={24} style={{ textAlign: 'center', marginBottom: '80px' }}>
+                        {user ? (
+                            <Chatbot />
+                        ) : (
+                            <Button
+                                type="primary"
+                                onClick={handleShowChat}
+                                style={{
+                                    padding: '10px 20px',
+                                    fontSize: '1.2rem',
+                                    backgroundColor: '#007000',
+                                    borderColor: '#007000',
+                                }}
+                            >
+                                Chat Now
+                            </Button>
+                        )}
                     </Col>
                 </Row>
-
             </Content>
-          
+            <Footer
+       style={{ textAlign: 'center', padding: '20px 50px', backgroundColor: '#EEE0CB', marginBottom: '80px' }}
+      >
+        BHBH Design ©{new Date().getFullYear()} Created by MJBH
+      </Footer>
         </Layout>
-       
-        
-       
-    )
-}
+    );
+};
 
-export default ChatPage
+export default ChatPage;
+
