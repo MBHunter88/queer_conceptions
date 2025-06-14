ffbdc3a4f4c2f48f3cbe88844c32d4d74fb83381
MBHunter88 <chellehunt88@gmail.com>
2025-06-13 21:39:58 -0700

    update script to use the correct path for the build script

--- server/railway-build.sh
+++ server/railway-build.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
+set -e
+set -x
 
 # Install and build the frontend
-cd client
+cd ../client
 npm install
 npm run build
-cd ..
 
-# Copy dist to server so it can be served
-rm -rf server/client-dist
-cp -r client/dist server/client-dist
+# Copy dist to server folder
+rm -rf ../server/client-dist
+cp -r dist ../server/client-dist
 
-# Install backend dependencies
-cd server
+# Go back to server and install backend dependencies
+cd ../server
 npm install
